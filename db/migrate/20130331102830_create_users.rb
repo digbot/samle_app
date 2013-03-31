8d7f1a353a04b1c701c0183d74dfcc7a1c6fc96e
Dimitar Dimitrov <dimitar.sashev@gmail.com>
2013-03-31 14:59:08 +0300

    Make a basic User model (including secure passwords)

---
+++ db/migrate/20130331102830_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+ 
+  def change
+    add_index :users, :email, unique: true
+  end
+end
