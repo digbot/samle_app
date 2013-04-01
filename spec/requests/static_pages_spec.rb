07e298071388210a2da998de0dac42b67b75e879
Dimitar Dimitrov <dimitar.sashev@gmail.com>
2013-04-01 10:24:12 +0300

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,21 +14,21 @@ describe "Static pages" do
     
     
     describe "for signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-        sign_in user
-        visit root_path
-      end
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+          FactoryGirl.create(:micropost, user: user, content: "Lorem")
+          FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+          sign_in user
+          visit root_path
+        end
 
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          page.should have_selector("li##{item.id}", text: item.content)
+        it "should render the user's feed" do
+          user.feed.each do |item|
+            page.should have_selector("li##{item.id}", text: item.content)
+          end
         end
       end
-    end    
-  end
+   end
 
   describe "Help page" do
     before { visit help_path }
